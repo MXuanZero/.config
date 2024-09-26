8efac7b0ad613b1e0666bd075b9d72236a626277
MXuanZero <MXuan_0@163.com>
2024-09-27 07:09:45 +0800

    update lvim config!

--- lvim/config.lua
+++ lvim/config.lua
@@ -26,7 +26,6 @@ lvim.leader = "space"
 -- normal
 lvim.keys.normal_mode["x"] = "\"_x"
 lvim.keys.normal_mode["X"] = "\"_X"
-lvim.keys.normal_mode["d"] = "\"_d"
 lvim.keys.normal_mode["dd"] = "\"_dd"
 lvim.keys.normal_mode["D"] = "\"_D"
 lvim.keys.normal_mode["y"] = "\"+y"
@@ -52,7 +51,7 @@ lvim.keys.insert_mode["<C-/>"] = "<ESC>gcc"
 -- visual
 lvim.keys.visual_mode["x"] = "\"_x"
 lvim.keys.visual_mode["X"] = "\"_X"
-lvim.keys.visual_mode["dd"] = "\"_dd"
+lvim.keys.visual_mode["dd"] = "\"_d"
 lvim.keys.visual_mode["D"] = "\"_D"
 lvim.keys.visual_mode["c"] = "\"+c"
 lvim.keys.visual_mode["C"] = "\"+C"
@@ -91,10 +90,10 @@ require('bookmarks').setup {
   -- sign_priority = 8,  --set bookmark sign priority to cover other sign
   save_file = vim.fn.expand "$HOME/.bookmarks", -- bookmarks save file path
   keywords =  {
-    ["@t"] = "☑️ ", -- mark annotation startswith @t ,signs this icon as `Todo`
-    ["@w"] = "⚠️ ", -- mark annotation startswith @w ,signs this icon as `Warn`
-    ["@f"] = "⛏ ", -- mark annotation startswith @f ,signs this icon as `Fix`
-    ["@n"] = " ", -- mark annotation startswith @n ,signs this icon as `Note`
+    ["@t"] = "", -- mark annotation startswith @t ,signs this icon as `Todo`
+    ["@w"] = "", -- mark annotation startswith @w ,signs this icon as `Warn`
+    ["@f"] = "", -- mark annotation startswith @f ,signs this icon as `Fix`
+    ["@n"] = "󰎚", -- mark annotation startswith @n ,signs this icon as `Note`
   },
   on_attach = function(bufnr)
     local bm = require "bookmarks"
