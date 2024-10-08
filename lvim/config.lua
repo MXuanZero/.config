3ee85bde58391e9187526cfeb163c5cdcb33f7b8
MXuanZero <MXuan_0@163.com>
2024-10-08 09:12:39 +0800

    style

--- lvim/config.lua
+++ lvim/config.lua
@@ -11,8 +11,8 @@ lvim.plugins = {
     opts = {},
     -- Optional dependencies
     dependencies = {
-       "nvim-treesitter/nvim-treesitter",
-       "nvim-tree/nvim-web-devicons"
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons"
     },
   }
 }
@@ -69,9 +69,9 @@ lvim.keys.visual_mode["<S-l>"] = "g_"
 lvim.builtin.telescope.theme = "center"
 
 lvim.builtin.terminal.execs = {
-  { nil, "<M-`>", "Float Terminal", "float", nil },
+  { nil, "<M-`>", "Float Terminal",      "float",      nil },
   { nil, "<M-1>", "Horizontal Terminal", "horizontal", 0.3 },
-  { nil, "<M-2>", "Vertical Terminal", "vertical", 0.4 },
+  { nil, "<M-2>", "Vertical Terminal",   "vertical",   0.4 },
 }
 
 lvim.builtin.treesitter.ensure_installed = {
@@ -89,7 +89,7 @@ lvim.builtin.treesitter.ensure_installed = {
 require('bookmarks').setup {
   -- sign_priority = 8,  --set bookmark sign priority to cover other sign
   save_file = vim.fn.expand "$HOME/.bookmarks", -- bookmarks save file path
-  keywords =  {
+  keywords = {
     ["@t"] = "", -- mark annotation startswith @t ,signs this icon as `Todo`
     ["@w"] = "", -- mark annotation startswith @w ,signs this icon as `Warn`
     ["@f"] = "", -- mark annotation startswith @f ,signs this icon as `Fix`
@@ -98,13 +98,13 @@ require('bookmarks').setup {
   on_attach = function(bufnr)
     local bm = require "bookmarks"
     local map = vim.keymap.set
-    map("n","mm",bm.bookmark_toggle) -- add or remove bookmark at current line
-    map("n","mi",bm.bookmark_ann) -- add or edit mark annotation at current line
-    map("n","mc",bm.bookmark_clean) -- clean all marks in local buffer
-    map("n","mn",bm.bookmark_next) -- jump to next mark in local buffer
-    map("n","mp",bm.bookmark_prev) -- jump to previous mark in local buffer
-    map("n","ml",bm.bookmark_list) -- show marked file list in quickfix window
-    map("n","mx",bm.bookmark_clear_all) -- removes all bookmarks
+    map("n", "mm", bm.bookmark_toggle)  -- add or remove bookmark at current line
+    map("n", "mi", bm.bookmark_ann)     -- add or edit mark annotation at current line
+    map("n", "mc", bm.bookmark_clean)   -- clean all marks in local buffer
+    map("n", "mn", bm.bookmark_next)    -- jump to next mark in local buffer
+    map("n", "mp", bm.bookmark_prev)    -- jump to previous mark in local buffer
+    map("n", "ml", bm.bookmark_list)    -- show marked file list in quickfix window
+    map("n", "mx", bm.bookmark_clear_all) -- removes all bookmarks
   end
 }
 
